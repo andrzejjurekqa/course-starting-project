08ed70f63023793d92210bab1891bef6a90cebb2
AJ <andrzej.jurek.qa@gmail.com>
2024-03-16 17:31:54 +0100

    modified:   playwright.config.ts 	modified:   tests/local-project.spec.ts

--- tests/local-project.spec.ts
+++ tests/local-project.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from "@playwright/test";
 
-test('Go to project', async ({ page }) => {
+test('Go and drag and drop', async ({ page }) => {
     await page.goto('http://localhost:3000');
     await page.locator('#title').fill('New Project');
     await page.locator('#description').fill('New Project');
@@ -12,4 +12,17 @@ test('Go to project', async ({ page }) => {
     await expect(page.locator('#finished-projects-list')).toContainText('New Project');
     await page.getByRole('heading', { name: 'New Project' }).dragTo(page.locator('#active-projects-list'))
     await expect(page.locator('#active-projects-list')).toContainText('New Project');
+});
+
+test.only('Go and do some bugs', async ({ page }) => {
+    await page.goto('http://localhost:3000');
+    await page.getByRole('button').click();
+    await page.on('dialog', async dialog => {
+        await expect(dialog.message()).toContain('Invalid input, please try again!');
+        await dialog.accept();
+    });
+    await page.locator('#title').fill('New Project');
+    await page.getByRole('button').click();
+    await page.locator('#description').fill('New Project');
+    await page.getByRole('button').click();
 });
